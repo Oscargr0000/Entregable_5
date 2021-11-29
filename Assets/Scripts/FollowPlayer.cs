6885fb9463ad3ebef6de3ca82562f3b676aeeab8
Oscargr0000 <oscargr0000@gmail.com>
2021-11-29 01:32:52 +0100

    me quiero morir :)

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject Player;
-    private Vector3 offset = new Vector3(0, 10, 0);
+    private Vector3 offset = new Vector3(10, 0, 0);
 
-    private void Uptade()
+    private void Update()
     {
         transform.position = Player.transform.position + offset;
     }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(0, 0, 30);
+    private float RandomY = Random.Range(-11f, 11f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnObstacle()
+    {
+        
+    }
+}
